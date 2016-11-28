5a615658abbdfaaec1588397de12ff7eeef3a6ef
morizooo <knnmrs999@gmail.com>
2016-11-28 21:55:09 +0900

    不要メソッド削除

--- src/main/scala/Exercise3/Exercise3.scala
+++ src/main/scala/Exercise3/Exercise3.scala
@@ -12,14 +12,6 @@ object Exercise3_1 {
     println(x)
   }
 
-  val x = List(1, 2, 3, 4, 5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
-
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
