310d248354b9639a2dcbefcbe77e8d2b966739e5
morizooo <knnmrs999@gmail.com>
2016-12-01 01:26:51 +0900

    問題文追加

--- src/main/scala/Exercise2/Exercise2.scala
+++ src/main/scala/Exercise2/Exercise2.scala
@@ -1,21 +1,26 @@
 package Exercise2
 
 object Exercise2_1 {
+  // n番目のフィボナッチ数列を取得するfibを実装せよ
   def fib(n: Int): Int = ???
 }
 
 object Exercise2_2 {
+  // Array[A]がソートされているか調べるisSortedを実装せよ
   def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
 }
 
 object Exercise2_3 {
+  // 引数2つの関数fが、fを部分的に適用する引数1つの関数に変換されるcurryを実装せよ
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = ???
 }
 
 object Exercise2_4 {
+  // curryによる変換を逆向きに行うuncurryを実装せよ。
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = ???
 }
 
 object Exercise2_5 {
+  // 2つの関数を合成する高階関数composeを実装せよ
   def compose[A, B, C](f: B => C, g: A => B): A => C = ???
 }
--- src/main/scala/Exercise3/Exercise3.scala
+++ src/main/scala/Exercise3/Exercise3.scala
@@ -1,5 +1,6 @@
 package Exercise3
 
+// 以下を実行すると結果はどうなるか
 object Exercise3_1 {
   def main(args: Array[String]): Unit = {
     val x = List(1, 2, 3, 4, 5) match {
@@ -18,98 +19,128 @@ object Exercise3_1 {
   }
 }
 
+// Listの最初の要素を削除するtailを実装せよ
 object Exercise3_2 {
   def tail[A](l: List[A]): List[A] = ???
 }
 
+// Listの最初の要素を別の値と置き換えるsetHeadを実装せよ
 object Exercise3_3 {
   def setHead[A](l: List[A], h: A): List[A] = ???
 }
 
+// リストのn個を削除するdropを実装せよ
 object Exercise3_4 {
   def drop[A](l: List[A], n: Int): List[A] = ???
 }
 
+// 述語とマッチする場合に限りListの要素までを削除するdropWhileを実装せよ
 object Exercise3_5 {
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 }
 
+// Listの末尾を除いたListを返すinitを実装せよ
 object Exercise3_6 {
   def init[A](l: List[A]): List[A] = ???
 }
 
+// 以下を実行するとどうなるか
 object Exercise3_8 {
   def main(args: Array[String]): Unit = {
     println(List.foldRight(Cons(1, Cons(2, Cons(3, Nil))), Nil: List[Int])(Cons(_, _)))
   }
 }
 
+// foldRigthを使って要素数を取得するlengthを実装せよ
 object Exercise3_9 {
   def length[A](l: List[A]): Int = ???
 }
 
+// 再帰を用いてListの処理を行うfoldLeftを実装せよ
 object Exercise3_10 {
+//  @annotation.tailrec
   def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
 }
 
 object Exercise3_11 {
+  // foldLeftを使って全要素の和を計算すsumを実装せよ
   def sum(l: List[Int]) = ???
 
+  // foldLeftを使って全要素の積を計算するproductを実装せよ
   def product(l: List[Double]) = ???
 
+  // foldLeftを使ってlengthを実装せよ
   def length[A](l: List[A]): Int = ???
 }
 
 object Exercise3_12 {
+  // リストの逆順を返すreverseを実装せよ
   def reverse[A](l: List[A]): List[A] = ???
 }
 
 object Exercise3_13 {
+  // foldLeftを使ってfoldRightを実装せよ
   def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B = ???
 
+  // foldRightを使ってfoldLeftを実装せよ
   def foldLeftViaFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
 }
 
 object Exercise3_14 {
-  def append[A](a1: List[A], a2: List[A]): List[A] = ???
+  // foldLeftを使ってappendを実装せよ
+  def appendViaFoldRight[A](a1: List[A], a2: List[A]): List[A] = ???
+
+  // foldRightを使ってappendを実装せよ
+  def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] = ???
 }
 
 object Exercise3_15 {
+  // 複数のリストからなるリストを１つのリストとして連結するconcatを実装せよ
   def concat[A](l: List[List[A]]): List[A] = ???
 }
 
 object Exercise3_16 {
+  // 各要素に1を足しわせるplusOneを実装せよ
   def plusOne(l: List[Int]): List[Int] = ???
 }
 
 object Exercise3_17 {
+  // Doubleの各要素をStringに変換するdoubleToStringを実装せよ
   def doubleToString(l: List[Double]): List[String] = ???
 }
 
 object Exercise3_18 {
+  // リストの各要素を展開し、かつリストの構造を保つmapを実装せよ
   def map[A, B](l: List[A])(f: A => B): List[B] = ???
 }
 
 object Exercise3_19 {
+  // 与えられた述語条件が満たされるまでリストから要素を削除するるfilterを実装せよ
   def filter[A](l: List[A])(f: A => Boolean): List[A] = ???
 }
 
 object Exercise3_20 {
+  // mapと同じような動きをするflatMapを実装せよ
   def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = ???
 }
 
 object Exercise3_21 {
+  // flatMapを用いてfilterを実装せよ
   def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] = ???
 }
 
 object Exercise3_22 {
+  // リストを２つ受け取り、対応する要素同士を足し合わせるaddPairwiseを実装せよ
   def addPairwise(a: List[Int], b: List[Int]): List[Int] = ???
 }
 
 object Exercise3_23 {
+  // addPairwiseの要素を一般化したzipWithを実装せよ
   def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] = ???
 }
 
+// Listに別のListがサブシーケンスを含んでいるか調べるhasSubsequenceを実装せよ
+// 特定のリストで始まっているか調べるstartsWithを実装して呼ぶとよい
 object Exercise3_24 {
   //  @annotation.tailrec
   def startsWith[A](l: List[A], prefix: List[A]): Boolean = ???
@@ -119,29 +150,38 @@ object Exercise3_24 {
 }
 
 object Exercise3_25 {
+  // 二分木のノード数を数えるsizeを実装せよ
   def size[A](t: Tree[A]): Int = ???
 }
 
 object Exercise3_26 {
+  // 二分木の最大の要素を返すmaximumを実装せよ
   def maximum(t: Tree[Int]): Int = ???
 }
 
 object Exercise3_27 {
+  // 二分木のルートから任意のLeafまでの最長パスを返すdepth関数を実装せよ
   def depth[A](t: Tree[A]): Int = ???
 }
 
 object Exercise3_28 {
+  // 二分木の各要素を特定の関数を使って変更するmap関数を記述せよ。
   def map[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
 }
 
 object Exercise3_29 {
+  // size,maximum,depth,mapを一般化し、それらの類似点を抽象化する新しいfold関数を記述せよ。
   def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = ???
 
+  // foldを使ってsizeを実装せよ
   def sizeViaFold[A](t: Tree[A]): Int = ???
 
+  // foldを使ってmaximumを実装せよ
   def maximumViaFold(t: Tree[Int]): Int = ???
 
+  // foldを使ってdepthを実装せよ
   def depthViaFold[A](t: Tree[A]): Int = ???
 
+  // foldを使ってmapを実装せよ
   def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
 }
