9ae1fc503a9fed6e217d03f43374934a977ecd0c
morizooo <knnmrs999@gmail.com>
2016-12-17 11:37:46 +0900

    4章の問題文を追加

--- src/main/scala/Exercise4/Option.scala
+++ src/main/scala/Exercise4/Option.scala
@@ -1,15 +1,20 @@
 package Exercise4
 
 sealed trait Option[+A] {
-  // Exercise4_1
+  // Exercise4_1 下記の関数を実装せよ
+  // OptionがNoneでない場合はfを適用する
   def map[B](f: A => B): Option[B] = ???
 
-  def getOrElse[B >: A](default: => B): B = ???
-
+  // OptionがNoneでない場合は、失敗する可能性があるfを適用する
   def flatMap[B](f: A => Option[B]): Option[B] = ???
 
+  // OptionがNoneの時に、指定したデフォルト値を適用する
+  def getOrElse[B >: A](default: => B): B = ???
+
+  // OptionがNoneだったら、指定したデフォルトのOption型を返す
   def orElse[B >: A](ob: => Option[B]): Option[B] = ???
 
+  // 値がfの条件を満たさない場合は、SomeをNoneに変換する
   def filter(f: A => Boolean): Option[A] = ???
 }
 
