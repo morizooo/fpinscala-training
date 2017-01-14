3eabc0d8c46e7d704f3868ac42b00521e7802e8c
morizooo <knnmrs999@gmail.com>
2017-01-14 12:08:35 +0900

    問題文のファイル統合

--- src/main/scala/Exercise5/Stream.scala
+++ src/main/scala/Exercise5/Stream.scala
@@ -67,3 +67,37 @@ trait Stream[+A] {
 case object Empty extends Stream[Nothing]
 
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+  // Exercise5_9 nで始まってn+1,n+2と続く整数の無限ストリームを生成する関数を実装せよ
+  def from(n: Int): Stream[Int] = ???
+
+  // Exercise5_10 フィボナッチ数列(0,1,1,2,3,5,8...)の無限ストリームを生成する関数を実装せよ
+  val fibs = ???
+
+  // Exercise5_11 汎用的なストリーム生成関数を実装せよ
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+
+  // Exercise5_12 unfoldを使ってfibs,from,constant,onesを実装せよ
+  val fibsViaUnfold = ???
+
+  def fromViaUnfold(n: Int) = ???
+
+  def constantViaUnfold[A](a: A) = ???
+
+  val onesViaUnfold = ???
+}
