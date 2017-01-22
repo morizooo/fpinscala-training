8f9511ff4ecf2c5a6b4fc8b5d3f651bbb5ebdb7e
m_dake <m_dake@septeni-original.co.jp>
2017-01-22 23:52:56 +0900

    5-4~5-7までをコミット。

--- src/test/scala/Exercise5/Exercise5Spec.scala
+++ src/test/scala/Exercise5/Exercise5Spec.scala
@@ -68,15 +68,68 @@ class Exercise5_3Spec extends Specification {
 }
 
 class Exercise5_4Spec extends Specification {
+  "Exercise5_4_forAll" should {
+    "全ての要素が条件に合致した場合" in {
+      Stream(1, 2, 3).forAll(n => n < 4) must be_==(true)
+    }
+    "一部の要素しか条件に合致しなかった場合" in {
+      Stream(1, 2, 3).forAll(n => n > 2) must be_==(false)
+    }
+    "全ての要素が条件に合致しなかった場合" in {
+      Stream(1, 2, 3).forAll(n => n > 4) must be_==(false)
+    }
+  }
 }
 
 class Exercise5_5Spec extends Specification {
+  "Exercise5_5_takeWhileViaFoldRight" should {
+    "マッチする要素を取り出せた場合" in {
+      Stream(1, 3, 2, 5).takeWhileViaFoldRight(n => n % 2 == 1).toList must be_==(List(1, 3))
+    }
+    "マッチする要素がなく取り出せなかった場合" in {
+      Stream(0).takeWhileViaFoldRight(n => n % 2 == 1).toList must be_==(List())
+    }
+  }
 }
 
 class Exercise5_6Spec extends Specification {
+  "Exercise5_6_headOptionViaFoldRight" should {
+    "先頭の要素を取り出せた場合" in {
+      Stream(1, 3, 2, 5).headOptionViaFoldRight.toList must be_==(List(1))
+    }
+    "要素がないStreamで先頭の要素を取り出せなかった場合" in {
+      Stream().headOptionViaFoldRight.toList must be_==(List())
+    }
+  }
 }
 
 class Exercise5_7Spec extends Specification {
+  "Exercise5_7_map" should {
+    "mapが適用できて、各要素を2倍できた場合" in {
+      Stream(1, 2, 3).map(n => n * 2).toList must be_==(List(2, 4, 6))
+    }
+  }
+  "Exercise5_7_filter" should {
+    "filterが適用できて、奇数の要素を抽出できた場合" in {
+      Stream(1, 2, 3, 4, 5).filter(n => n % 2 == 1).toList must be_==(List(1, 3, 5))
+    }
+  }
+  "Exercise5_7_append" should {
+    "Streamの結合ができた場合" in {
+      Stream(1, 2, 3, 4, 5).append(Stream(6, 7)).toList must be_==(List(1, 2, 3, 4, 5, 6, 7))
+    }
+    "空Streamとの結合ができた場合" in {
+      Stream(1, 2, 3, 4, 5).append(Stream()).toList must be_==(List(1, 2, 3, 4, 5))
+    }
+    "Streamの要素の型が違った場合" in {
+      Stream(1, 2, 3, 4, 5).append(Stream("a")).toList must be_==(List(1, 2, 3, 4, 5, "a"))
+    }
+  }
+  "Exercise5_7_flatmap" should {
+    "flatmapが適用できて、一部の要素に'A'(65)を足した要素を追加できた場合" in {
+      Stream(0, 1, 2).flatMap(n => Stream(n, 'A' + n)).toList must be_==(List(0, 65, 1, 66, 2, 67))
+    }
+  }
 }
 
 class Exercise5_8Spec extends Specification {
