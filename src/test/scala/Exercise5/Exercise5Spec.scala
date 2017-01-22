33bc11b488edee7f48e7ec868f7f98ff9a12034b
m_dake <m_dake@septeni-original.co.jp>
2017-01-22 19:38:28 +0900

    スケルトンをコミット。

---
+++ src/test/scala/Exercise5/Exercise5Spec.scala
@@ -0,0 +1,36 @@
+package Exercise5
+
+import org.specs2.mutable.Specification
+
+class Exercise5_1Spec extends Specification {
+}
+class Exercise5_2Spec extends Specification {
+}
+class Exercise5_3Spec extends Specification {
+}
+class Exercise5_4Spec extends Specification {
+}
+class Exercise5_5Spec extends Specification {
+}
+class Exercise5_6Spec extends Specification {
+}
+class Exercise5_7Spec extends Specification {
+}
+class Exercise5_8Spec extends Specification {
+}
+class Exercise5_9Spec extends Specification {
+}
+class Exercise5_10Spec extends Specification {
+}
+class Exercise5_11Spec extends Specification {
+}
+class Exercise5_12Spec extends Specification {
+}
+class Exercise5_13Spec extends Specification {
+}
+//class Exercise5_14Spec extends Specification {
+//}
+class Exercise5_15Spec extends Specification {
+}
+//class Exercise5_16Spec extends Specification {
+//}
